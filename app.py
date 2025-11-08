2b9de751449a79f141cf9b5f1ad92f3255397f8b
Samara Porto Souza <samaraporto1301@gmail.com>
2025-11-08 12:25:27 -0300

    Fix indentation in getConnection function

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ CORS(app)
 # ==============================================================
 def getConnection():
     try:
-    return oracledb.connect(
-        user="rm559072",
-        password="130106",
-        host="oracle.fiap.com.br",
-        port=1521,
-        service_name="orcl"
-
+        return oracledb.connect(
+            user="rm559072",
+            password="130106",
+            host="oracle.fiap.com.br",
+            port=1521,
+            service_name="orcl"
+    
 )
 
 
